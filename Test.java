6d5609d44972e388d398b0c5efe20da5b7d4e213
MochDedy <mochdedy96@gmail.com>
2025-09-25 13:05:02 +0700

    Berkenalan dengan surya

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        
+        System.out.println("Halo Surya ini aku Dedy");
+        System.out.println("Salam kenal!");
+    }
+    
+}
