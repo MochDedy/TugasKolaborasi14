7024e2451b0c18f47f8da24bf2e53d926e116aad
l4utan <espressvre29@gmail.com>
2025-09-25 13:13:02 +0700

    dedy tampan

--- Surya.java
+++ Surya.java
@@ -1,6 +1,6 @@
 public class Surya {
     public static void main(String[] args) {
         System.out.println("Aku Surya");
-        System.out.println("Aku tampan");
+        // System.out.println("Aku tampan");
     }
 }
--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         
         System.out.println("Halo Surya ini aku Dedy");
-        System.out.println("Salam kenal!");
+        // System.out.println("Salam kenal!");
     }
     
 }
